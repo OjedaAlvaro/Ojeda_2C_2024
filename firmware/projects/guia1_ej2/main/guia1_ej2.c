09dfecb4ddb82646e957115cfe97c9ade9e48fc1
OjedaAlvaro <ojedaalvaro96@gmail.com>
2024-07-31 11:07:27 -0300

    Agregue ejercicio 2

--- firmware/projects/guia1_ej2/main/guia1_ej2.c
+++ firmware/projects/guia1_ej2/main/guia1_ej2.c
@@ -1,16 +1,26 @@
-/*! @mainpage Blinking switch
+/*! @mainpage Ejercicio 2
  *
- * \section genDesc General Description
+ * @section genDesc General Description
  *
- * This example makes LED_1 and LED_2 blink if SWITCH_1 or SWITCH_2 are pressed.
+ * Este programa hace titilar los led 1 y 2 apretando las teclas 1 y 2 respectivamente, si apretamos
+ * ambas teclas titila el led 3
+ *
+ * <a href="https://drive.google.com/...">Operation Example</a>
+ *
+ * @section hardConn Hardware Connection
+ *
+ * |    Peripheral  |   ESP32   	|
+ * |:--------------:|:--------------|
+ * | 	PIN_X	 	| 	GPIO_X		|
+ *
  *
  * @section changelog Changelog
  *
  * |   Date	    | Description                                    |
  * |:----------:|:-----------------------------------------------|
- * | 12/09/2023 | Document creation		                         |
+ * | 31/07/2024 | Document creation		                         |
  *
- * @author Albano Peñalva (albano.penalva@uner.edu.ar)
+ * @author Alvaro Ojeda (alvaro.ojeda@uner.edu.ar)
  *
  */
 
@@ -22,8 +32,10 @@
 #include "freertos/task.h"
 #include "led.h"
 #include "switch.h"
+
+#define CONFIG_BLINK_PERIOD 250
 /*==================[macros and definitions]=================================*/
-#define CONFIG_BLINK_PERIOD 1000
+
 /*==================[internal data definition]===============================*/
 
 /*==================[internal functions declaration]=========================*/
@@ -33,6 +45,7 @@ void app_main(void){
 	uint8_t teclas;
 	LedsInit();
 	SwitchesInit();
+	const uint8_t tecla3 = SWITCH_1 |SWITCH_2;
     while(1)    {
     	teclas  = SwitchesRead();
     	switch(teclas){
@@ -42,10 +55,14 @@ void app_main(void){
     		case SWITCH_2:
     			LedToggle(LED_2);
     		break;
+
+			case tecla3:
+    			LedToggle(LED_3);
+    		break;
 			
 
     	}
-	    LedToggle(LED_3);
 		vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
 	}
 }
+/*==================[end of file]============================================*/
